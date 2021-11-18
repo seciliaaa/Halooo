dd53c74e7c5a4f74df286d138c4224ce41b1724a
seciliaaa <94610473+seciliaaa@users.noreply.github.com>
2021-11-18 21:08:30 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,10 @@
+Nama = "Secilia Karunia Karen"
+Nim = "212410102050"
+Prodi = "Teknologi Informasi"
+Fakultas = "Ilmu Komputer"
+Asal = "Lumajang"
+Hobi = "Berenang"
+
+print("Hallooo, kenalin namaku", Nama, "dengan NIM", Nim, "Prodi", Prodi, "Fakultas", Fakultas)
+print("Aku berasal dari Kabupaten", Asal, "dan hobiku", Hobi)
+print("Salam kenal semuanyaaaa") 
